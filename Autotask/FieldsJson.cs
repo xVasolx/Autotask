d8cf2b2240c767570f2f24f4ce1679ec445e282a
Den <135476446+xVasolx@users.noreply.github.com>
2023-07-04 15:59:28 +1000

    Создано клиентское подключение с авторизацией и преобразованием json

---
+++ Autotask/FieldsJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotask
+{
+    internal class FieldsJson
+    {
+        public int id { get; set; }
+        public string project_Name { get; set; }
+        public string project_Description { get; set; }
+        public string project_Owner { get; set; }
+        public string project_DateAdd { get; set; }
+        //public object[]? projectVersion { get; set; }
+    }
+}
---
+++ Autotask/ParseJson.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotask
+{
+    internal class ParseJson
+    {
+        private readonly string jsonFile;
+        public ParseJson(string jsonFile)
+        {
+            this.jsonFile = jsonFile;
+        }
+        public List<FieldsJson> UseUserDefinedObjectWithNewtonsoftJson()
+        {
+            List<FieldsJson> fileInfo = JsonConvert.DeserializeObject<List<FieldsJson>>(jsonFile);
+            return fileInfo;
+        }
+    }
+}
